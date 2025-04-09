22460831e6aef649813d715e5e9554b645ff77f3
ericnabofa <111744463+ericnabofa@users.noreply.github.com>
2025-04-09 17:19:38 +0100

    Adds hosting details

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your backend URL
+  baseURL: 'http://185.113.249.229:5000/', // Replace with your backend URL
 });
 
 export default api;
