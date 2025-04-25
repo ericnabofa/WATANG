8391579d2db8ff26fbf7514305ba49115a26ce4f
ericnabofa <111744463+ericnabofa@users.noreply.github.com>
2025-04-25 08:07:34 +0100

    adds vercel details to frontend hosting

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://185.113.249.229:5000/', // Replace with your backend URL
+  baseURL: import.meta.env.VITE_API_URL, // Replace with your backend URL
 });
 
 export default api;
